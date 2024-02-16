fbb19d940095873a3ad7ad3feb035ee678b4e3ce
Rantsa-B613 <105979770+Rantsa-B613@users.noreply.github.com>
2024-02-16 11:51:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+import 'package:nahandroko/screens/HomeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,48 +16,10 @@ class MyApp extends StatelessWidget {
       title: 'nahandroko',
       theme: ThemeData(
         primaryColor:
-            Colors.blue, // Vous pouvez définir votre propre couleur ici
+            Colors.yellow, // Vous pouvez définir votre propre couleur ici
       ),
       home: HomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  String message = '';
-
-  Future<void> fetchData() async {
-    final response = await http.get(Uri.parse('http://votre_ip:port/api/data'));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      setState(() {
-        message = data['message'];
-      });
-    } else {
-      throw Exception('Failed to load data');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Votre Application'),
-      ),
-      body: Center(
-        child: Text(message),
-      ),
-    );
-  }
-}
